e0ccd46292c772342db05949579015575c00e8a4
Sheck Obidullah <65061871+obidullah01@users.noreply.github.com>
2021-03-02 00:28:04 +0600

    Address Class created

---
+++ Address.cs
@@ -0,0 +1,21 @@
+﻿namespace Banking_Management
+{
+    class Address
+    {
+        private string roadNo;
+        private string houseNo;
+        private string city;
+        private string country;
+        public Address(string roadNo, string houseNo, string city, string country)
+        {
+            this.country = country;
+            this.houseNo = houseNo;
+            this.city = city;
+            this.roadNo = roadNo;
+        }
+        public String GetAddress()
+        {
+            return "Address is => " + "Road No: " + roadNo + ", House No: " + houseNo + ", City: " + city + ", Country: " + country;
+        }
+    }
+}
